5cc4d42adbd0c8bda35f2f50fe7d56d447176871
ludens-gc <81386656+ludens-gc@users.noreply.github.com>
2024-08-22 18:57:02 -0300

    Initial commit
    Generated by create-expo-app 3.0.0.

---
+++ prettier.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  // Define largura máxima da linha
+  printWidth: 80,
+
+  // Utiliza aspas simples em vez de aspas duplas
+  singleQuote: true,
+
+  // Adiciona ponto e vírgula no final das linhas
+  semi: true,
+
+  // Utiliza tabulação em vez de espaços
+  useTabs: false,
+
+  // Define tamanho da indentação como 2 espaços
+  tabWidth: 2,
+
+  // Adiciona vírgula ao final de objetos e arrays
+  trailingComma: 'es5',
+
+  // Insere um espaço entre colchetes e o conteúdo dentro dos objetos
+  bracketSpacing: true,
+
+  // Coloca o fechamento dos JSX na mesma linha
+  jsxBracketSameLine: false,
+
+  // Parênteses ao redor de argumentos de arrow functions
+  arrowParens: 'always',
+
+  // Define o estilo de final de linha como 'auto'
+  endOfLine: 'auto',
+
+  // Formata automaticamente blocos `@format` ou arquivos que contêm o pragma
+  requirePragma: false,
+
+  // Insere o pragma @format no topo dos arquivos formatados
+  insertPragma: false,
+
+  // Controla a largura do texto em markdown
+  proseWrap: 'preserve',
+};
